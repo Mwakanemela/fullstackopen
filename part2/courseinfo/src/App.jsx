3e315af995a781ecff33ad726e423d4dee18de84
Mwakanemela <kayangemwakanemela@gmail.com>
2025-09-22 01:36:50 -0700

    2.4: Course information step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,62 @@
 import Course from "./components/Course"
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4
-      }
-    ],
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+    
+  
 
-  return <Course course={course} />
+  return (
+    <div>
 
-  //  <Total
-  //       total={
-  //         course.parts[0].exercises +
-  //         course.parts[1].exercises +
-  //         course.parts[2].exercises
-  //       }
-  //     />
+      {courses.map(course => <Course key={course.id} course={course} />)}
 
-  // const Total = (props) => <p>Number of exercises {props.total}</p>
+      
+    </div>
+  )
+
 }
 
 export default App
