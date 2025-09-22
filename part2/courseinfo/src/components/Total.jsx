eb3350afdb77665f6223ddc46fb3441655452e2d
Mwakanemela <kayangemwakanemela@gmail.com>
2025-09-22 01:27:27 -0700

    2.3*: Course information step 8

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -2,11 +2,18 @@ const Total = (props) => {
 
     const {parts} = props
 
+    // YOU CAN MAP
     let sum = 0
-    const total = parts.map(part => sum +=part.exercises  )
+    parts.map(part => sum +=part.exercises  )
 
+    // OR
+
+    // REDUCE
+    const total = parts.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+    
     return (
-        <p><b>total of {sum} exercises</b> </p>
+        <p><b>total of {total} exercises</b> </p>
     )
     
 }
