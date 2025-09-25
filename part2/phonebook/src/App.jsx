d3301ac1b59656306025fa8921a52e8654291136
Mwakanemela <kayangemwakanemela@gmail.com>
2025-09-25 08:28:17 -0700

    2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,28 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
 import DisplayPhoneBookUsers from './components/Display'
 import Filter from "./components/Filter"
 
 const App = () => {
 
+  useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
   // FOR ADDING PERSONS(PEOPLE)
-  const [persons, setPersons] = useState([
-    { 
-      phoneNumber: 1234,
-      name: 'Arto Hellas',
-      id: 1
-     },
-     { 
-      phoneNumber: 9876,
-      name: 'Mwakanemela Kayange',
-      id: 2
-     },
-     { 
-      phoneNumber: 4567,
-      name: 'Hello World',
-      id: 3
-     }
-  ]) 
+  const [persons, setPersons] = useState([ ]) 
   const addPerson = (newPerson) => {
     
     setPersons([...persons, newPerson]);
