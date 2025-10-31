94cf14fe45fe638d980507e623f93e31e15b4a5a
Mwakanemela <kayangemwakanemela@gmail.com>
2025-10-31 12:53:58 -0700

    3.2: Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,6 +14,14 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook_data)
 })
 
+app.get('/api/info', (request, response) => {
+    const phonebook_data_size = phonebook_data.length
+    const now = new Date();
+
+    response.type('text/plain');
+    response.send(`Phonebook has info for ${phonebook_data_size} people \n${now.toString()}`)
+    
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
