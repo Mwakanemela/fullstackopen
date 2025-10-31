6ab3d84616ffa2c12c9dfb7232c5a22be7262d7e
Mwakanemela <kayangemwakanemela@gmail.com>
2025-10-31 13:16:52 -0700

    3.4: Phonebook backend step 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,11 +9,12 @@ app.get('/', (request, response) => {
   response.send('<h1>Access Denied!</h1>')
 })
 
-
+//get all persons 
 app.get('/api/persons', (request, response) => {
   response.json(phonebook_data)
 })
 
+//get all persons info and visitor datetime now
 app.get('/api/info', (request, response) => {
     const phonebook_data_size = phonebook_data.length
     const now = new Date();
@@ -23,6 +24,7 @@ app.get('/api/info', (request, response) => {
     
 })
 
+//get single person data
 app.get("/api/persons/:id", (request, response) => {
 
   const person_id = request.params.id
@@ -34,6 +36,14 @@ app.get("/api/persons/:id", (request, response) => {
   }
 
   response.json(person)
+})
+
+//delete single user
+app.delete('/api/persons/:id', (request, response) => {
+  const person_id = request.params.id
+  person_to_delete = phonebook_data.filter(person => person.id !== person_id)
+  // console.log(person_to_delete)
+  response.status(204).end()
 })
 const PORT = 3001
 app.listen(PORT, () => {
